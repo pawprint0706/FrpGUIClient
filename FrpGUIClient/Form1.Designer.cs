a0b8e872a226bc487802c3cecb54d42a66ed01f5
Youngchan Kim <yckim@suhotech.com>
2024-12-11 16:20:01 +0900

    최초 구현

--- FrpGUIClient/Form1.Designer.cs
+++ FrpGUIClient/Form1.Designer.cs
@@ -2,15 +2,23 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// 필수 디자이너 변수입니다.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label labelDeviceName;
+        private System.Windows.Forms.TextBox textBoxDeviceName;
+        private System.Windows.Forms.Label labelServerAddr;
+        private System.Windows.Forms.TextBox textBoxServerAddr;
+        private System.Windows.Forms.Label labelServerPort;
+        private System.Windows.Forms.TextBox textBoxServerPort;
+        private System.Windows.Forms.Label labelAuthToken;
+        private System.Windows.Forms.TextBox textBoxAuthToken;
+        private System.Windows.Forms.Label labelLocalPort;
+        private System.Windows.Forms.TextBox textBoxLocalPort;
+        private System.Windows.Forms.Label labelRemotePort;
+        private System.Windows.Forms.TextBox textBoxRemotePort;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
 
-        /// <summary>
-        /// 사용 중인 모든 리소스를 정리합니다.
-        /// </summary>
-        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +28,179 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form 디자이너에서 생성한 코드
-
-        /// <summary>
-        /// 디자이너 지원에 필요한 메서드입니다. 
-        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.labelDeviceName = new System.Windows.Forms.Label();
+            this.textBoxDeviceName = new System.Windows.Forms.TextBox();
+            this.labelServerAddr = new System.Windows.Forms.Label();
+            this.textBoxServerAddr = new System.Windows.Forms.TextBox();
+            this.labelServerPort = new System.Windows.Forms.Label();
+            this.textBoxServerPort = new System.Windows.Forms.TextBox();
+            this.labelAuthToken = new System.Windows.Forms.Label();
+            this.textBoxAuthToken = new System.Windows.Forms.TextBox();
+            this.labelLocalPort = new System.Windows.Forms.Label();
+            this.textBoxLocalPort = new System.Windows.Forms.TextBox();
+            this.labelRemotePort = new System.Windows.Forms.Label();
+            this.textBoxRemotePort = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // labelDeviceName
+            // 
+            this.labelDeviceName.AutoSize = true;
+            this.labelDeviceName.Location = new System.Drawing.Point(12, 15);
+            this.labelDeviceName.Name = "labelDeviceName";
+            this.labelDeviceName.Size = new System.Drawing.Size(131, 12);
+            this.labelDeviceName.TabIndex = 0;
+            this.labelDeviceName.Text = "장치명(Device Name):";
+            // 
+            // textBoxDeviceName
+            // 
+            this.textBoxDeviceName.Location = new System.Drawing.Point(184, 12);
+            this.textBoxDeviceName.Name = "textBoxDeviceName";
+            this.textBoxDeviceName.Size = new System.Drawing.Size(310, 21);
+            this.textBoxDeviceName.TabIndex = 1;
+            // 
+            // labelServerAddr
+            // 
+            this.labelServerAddr.AutoSize = true;
+            this.labelServerAddr.Location = new System.Drawing.Point(12, 42);
+            this.labelServerAddr.Name = "labelServerAddr";
+            this.labelServerAddr.Size = new System.Drawing.Size(158, 12);
+            this.labelServerAddr.TabIndex = 2;
+            this.labelServerAddr.Text = "서버 주소(Server Address):";
+            // 
+            // textBoxServerAddr
+            // 
+            this.textBoxServerAddr.Location = new System.Drawing.Point(184, 39);
+            this.textBoxServerAddr.Name = "textBoxServerAddr";
+            this.textBoxServerAddr.Size = new System.Drawing.Size(310, 21);
+            this.textBoxServerAddr.TabIndex = 3;
+            // 
+            // labelServerPort
+            // 
+            this.labelServerPort.AutoSize = true;
+            this.labelServerPort.Location = new System.Drawing.Point(12, 69);
+            this.labelServerPort.Name = "labelServerPort";
+            this.labelServerPort.Size = new System.Drawing.Size(133, 12);
+            this.labelServerPort.TabIndex = 4;
+            this.labelServerPort.Text = "서버 포트(Server Port):";
+            // 
+            // textBoxServerPort
+            // 
+            this.textBoxServerPort.Location = new System.Drawing.Point(184, 66);
+            this.textBoxServerPort.Name = "textBoxServerPort";
+            this.textBoxServerPort.Size = new System.Drawing.Size(310, 21);
+            this.textBoxServerPort.TabIndex = 5;
+            // 
+            // labelAuthToken
+            // 
+            this.labelAuthToken.AutoSize = true;
+            this.labelAuthToken.Location = new System.Drawing.Point(12, 96);
+            this.labelAuthToken.Name = "labelAuthToken";
+            this.labelAuthToken.Size = new System.Drawing.Size(135, 12);
+            this.labelAuthToken.TabIndex = 6;
+            this.labelAuthToken.Text = "인증 토큰(Auth Token):";
+            // 
+            // textBoxAuthToken
+            // 
+            this.textBoxAuthToken.Location = new System.Drawing.Point(184, 93);
+            this.textBoxAuthToken.Name = "textBoxAuthToken";
+            this.textBoxAuthToken.PasswordChar = '*';
+            this.textBoxAuthToken.Size = new System.Drawing.Size(310, 21);
+            this.textBoxAuthToken.TabIndex = 7;
+            // 
+            // labelLocalPort
+            // 
+            this.labelLocalPort.AutoSize = true;
+            this.labelLocalPort.Location = new System.Drawing.Point(12, 123);
+            this.labelLocalPort.Name = "labelLocalPort";
+            this.labelLocalPort.Size = new System.Drawing.Size(128, 12);
+            this.labelLocalPort.TabIndex = 8;
+            this.labelLocalPort.Text = "로컬 포트(Local Port):";
+            // 
+            // textBoxLocalPort
+            // 
+            this.textBoxLocalPort.Location = new System.Drawing.Point(184, 120);
+            this.textBoxLocalPort.Name = "textBoxLocalPort";
+            this.textBoxLocalPort.Size = new System.Drawing.Size(310, 21);
+            this.textBoxLocalPort.TabIndex = 9;
+            // 
+            // labelRemotePort
+            // 
+            this.labelRemotePort.AutoSize = true;
+            this.labelRemotePort.Location = new System.Drawing.Point(12, 150);
+            this.labelRemotePort.Name = "labelRemotePort";
+            this.labelRemotePort.Size = new System.Drawing.Size(140, 12);
+            this.labelRemotePort.TabIndex = 10;
+            this.labelRemotePort.Text = "원격 포트(Remote Port):";
+            // 
+            // textBoxRemotePort
+            // 
+            this.textBoxRemotePort.Location = new System.Drawing.Point(184, 147);
+            this.textBoxRemotePort.Name = "textBoxRemotePort";
+            this.textBoxRemotePort.Size = new System.Drawing.Size(310, 21);
+            this.textBoxRemotePort.TabIndex = 11;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(150, 190);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(88, 25);
+            this.buttonStart.TabIndex = 12;
+            this.buttonStart.Text = "시작(Start)";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(266, 190);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(88, 25);
+            this.buttonStop.TabIndex = 13;
+            this.buttonStop.Text = "중지(Stop)";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxOutput.Location = new System.Drawing.Point(14, 230);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.Size = new System.Drawing.Size(480, 208);
+            this.richTextBoxOutput.TabIndex = 14;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(508, 450);
+            this.Controls.Add(this.richTextBoxOutput);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxRemotePort);
+            this.Controls.Add(this.labelRemotePort);
+            this.Controls.Add(this.textBoxLocalPort);
+            this.Controls.Add(this.labelLocalPort);
+            this.Controls.Add(this.textBoxAuthToken);
+            this.Controls.Add(this.labelAuthToken);
+            this.Controls.Add(this.textBoxServerPort);
+            this.Controls.Add(this.labelServerPort);
+            this.Controls.Add(this.textBoxServerAddr);
+            this.Controls.Add(this.labelServerAddr);
+            this.Controls.Add(this.textBoxDeviceName);
+            this.Controls.Add(this.labelDeviceName);
+            this.Name = "Form1";
+            this.Text = "frp GUI Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
-        #endregion
     }
 }
-
--- FrpGUIClient/Form1.cs
+++ FrpGUIClient/Form1.cs
@@ -3,18 +3,415 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
 
 namespace FrpGUIClient
 {
     public partial class Form1 : Form
     {
+        private Process frpProcess = null;
+        private BackgroundWorker bgWorker = new BackgroundWorker();
+        private bool stopRequested = false;
+        private int maxRetries = 10;
+
         public Form1()
         {
             InitializeComponent();
+
+            bgWorker.WorkerSupportsCancellation = true;
+            bgWorker.DoWork += BgWorker_DoWork;
+            bgWorker.ProgressChanged += BgWorker_ProgressChanged;
+            bgWorker.RunWorkerCompleted += BgWorker_RunWorkerCompleted;
+            bgWorker.WorkerReportsProgress = true;
+
+            LoadConfig();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (frpProcess != null && !frpProcess.HasExited)
+            {
+                var result = MessageBox.Show("현재 연결이 끊어집니다. 종료하시겠습니까?", "종료 확인", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    StopFrp();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void LoadConfig()
+        {
+            if (File.Exists("frpc.ini"))
+            {
+                var lines = File.ReadAllLines("frpc.ini", Encoding.UTF8);
+                string section = "";
+                foreach (var line in lines)
+                {
+                    if (string.IsNullOrWhiteSpace(line)) continue;
+                    if (line.StartsWith("[") && line.EndsWith("]"))
+                    {
+                        section = line.Trim('[', ']');
+                        continue;
+                    }
+
+                    var parts = line.Split(new char[] { '=' }, 2);
+                    if (parts.Length == 2)
+                    {
+                        string key = parts[0].Trim();
+                        string val = parts[1].Trim();
+
+                        if (section == "common")
+                        {
+                            if (key == "server_addr") textBoxServerAddr.Text = val;
+                            if (key == "server_port") textBoxServerPort.Text = val;
+                            if (key == "token") textBoxAuthToken.Text = val;
+                        }
+                        else
+                        {
+                            // 첫 번째 프록시 섹션
+                            textBoxDeviceName.Text = section;
+                            if (key == "local_port") textBoxLocalPort.Text = val;
+                            if (key == "remote_port") textBoxRemotePort.Text = val;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (frpProcess != null && !frpProcess.HasExited)
+            {
+                MessageBox.Show("frpc가 이미 실행 중입니다.", "경고");
+                return;
+            }
+
+            // 필수 입력값 체크
+            if (string.IsNullOrWhiteSpace(textBoxServerAddr.Text))
+            {
+                MessageBox.Show("서버 주소를 입력하세요.", "오류");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textBoxServerPort.Text))
+            {
+                MessageBox.Show("서버 포트를 입력하세요.", "오류");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textBoxLocalPort.Text))
+            {
+                MessageBox.Show("로컬 포트를 입력하세요.", "오류");
+                return;
+            }
+
+            if (!ValidatePort(textBoxServerPort.Text))
+            {
+                MessageBox.Show("서버 포트는 1에서 65535 사이의 숫자여야 합니다.", "오류");
+                return;
+            }
+            if (!ValidatePort(textBoxLocalPort.Text))
+            {
+                MessageBox.Show("로컬 포트는 1에서 65535 사이의 숫자여야 합니다.", "오류");
+                return;
+            }
+
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+            stopRequested = false;
+
+            if (!bgWorker.IsBusy)
+            {
+                richTextBoxOutput.Clear();
+                bgWorker.RunWorkerAsync();
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            StopFrp();
+        }
+
+        private void StopFrp()
+        {
+            stopRequested = true;
+            if (frpProcess != null && !frpProcess.HasExited)
+            {
+                try
+                {
+                    frpProcess.Kill();
+                    frpProcess.WaitForExit();
+                }
+                catch { }
+            }
+            frpProcess = null;
+            richTextBoxOutput.AppendText("frpc가 중지되었습니다.\n");
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+        private bool ValidatePort(string portStr)
+        {
+            int port;
+            if (int.TryParse(portStr, out port))
+            {
+                if (port >= 1 && port <= 65535) return true;
+            }
+            return false;
+        }
+
+        private int GetValidPortOrRandom(string portStr)
+        {
+            if (string.IsNullOrWhiteSpace(portStr))
+            {
+                // 랜덤 포트 할당
+                var rand = new Random();
+                return rand.Next(10000, 65535);
+            }
+
+            int port;
+            if (int.TryParse(portStr, out port))
+            {
+                if (port >= 1 && port <= 65535) return port;
+            }
+            return -1; // invalid
+        }
+
+        private string GenerateProxyName()
+        {
+            var name = textBoxDeviceName.Text.Trim().Replace(" ", "");
+            if (string.IsNullOrEmpty(name))
+            {
+                var rand = new Random();
+                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                name = new string(Enumerable.Repeat(chars, 10).Select(s => s[rand.Next(s.Length)]).ToArray());
+            }
+            return name;
+        }
+
+        private void GenerateConfig(int remotePort, string proxyName)
+        {
+            // frpc.ini 파일 생성
+            using (var sw = new StreamWriter("frpc.ini", false, Encoding.UTF8))
+            {
+                sw.WriteLine("[common]");
+                sw.WriteLine($"server_addr = {textBoxServerAddr.Text.Trim()}");
+                sw.WriteLine($"server_port = {textBoxServerPort.Text.Trim()}");
+                sw.WriteLine($"token = {textBoxAuthToken.Text.Trim()}");
+                sw.WriteLine();
+                sw.WriteLine($"[{proxyName}]");
+                sw.WriteLine("type = tcp");
+                sw.WriteLine($"local_port = {textBoxLocalPort.Text.Trim()}");
+                sw.WriteLine($"remote_port = {remotePort}");
+            }
+        }
+
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int retries = 0;
+            bool portAssigned = false;
+
+            while (retries < maxRetries && !portAssigned && !stopRequested)
+            {
+                int remotePort = GetValidPortOrRandom(textBoxRemotePort.Text.Trim());
+                if (remotePort == -1)
+                {
+                    // 유효하지 않은 포트 입력 시 바로 중단
+                    ReportLog("원격 포트는 1에서 65535 사이의 숫자여야 합니다.\n");
+                    break;
+                }
+
+                string proxyName = GenerateProxyName();
+                GenerateConfig(remotePort, proxyName);
+
+                // frpc 실행 파일 경로 설정 (현재 디렉토리 가정)
+                string frpExecutable = "frpc.exe";
+                string frpFullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, frpExecutable);
+
+                if (!File.Exists(frpFullPath))
+                {
+                    ReportLog($"frp 실행 파일을 찾을 수 없습니다: {frpFullPath}\n");
+                    break;
+                }
+
+                ReportLog($"원격 포트(remote_port): {remotePort}로 시도 중...\n");
+
+                // 기존 프로세스 종료
+                if (frpProcess != null && !frpProcess.HasExited)
+                {
+                    try
+                    {
+                        frpProcess.Kill();
+                        frpProcess.WaitForExit();
+                    }
+                    catch { }
+                }
+
+                var psi = new ProcessStartInfo
+                {
+                    FileName = frpFullPath,
+                    Arguments = "-c frpc.ini",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    StandardOutputEncoding = Encoding.UTF8,
+                    StandardErrorEncoding = Encoding.UTF8
+                };
+
+                frpProcess = Process.Start(psi);
+
+                bool portInUse = false;
+                bool startSuccess = false;
+
+                if (frpProcess != null)
+                {
+                    // 초기 출력 모니터링
+                    while (!frpProcess.HasExited)
+                    {
+                        if (bgWorker.CancellationPending || stopRequested)
+                            break;
+                        string line = frpProcess.StandardOutput.ReadLine();
+                        if (line == null) break;
+
+                        HandleLogLine(line);
+                        if (line.Contains("port already used") ||
+                            line.Contains("the proxy is already in use") ||
+                            Regex.IsMatch(line, "proxy .* already exists"))
+                        {
+                            portInUse = true;
+                            break;
+                        }
+                        if (line.Contains("start proxy success"))
+                        {
+                            startSuccess = true;
+                            portAssigned = true;
+                            break;
+                        }
+                        if (line.Contains("start error"))
+                        {
+                            portInUse = true;
+                            break;
+                        }
+                    }
+
+                    if (portInUse)
+                    {
+                        ReportLog($"포트 {remotePort}는 이미 사용 중입니다. 다른 포트로 재시도합니다.\n");
+                        try
+                        {
+                            frpProcess.Kill();
+                            frpProcess.WaitForExit();
+                        }
+                        catch { }
+                        retries++;
+                        // 다음 루프에서 랜덤 포트
+                        this.Invoke((Action)(() => { textBoxRemotePort.Text = ""; }));
+                        continue;
+                    }
+                    else if (!startSuccess)
+                    {
+                        // 다른 에러
+                        try
+                        {
+                            frpProcess.Kill();
+                            frpProcess.WaitForExit();
+                        }
+                        catch { }
+                        break;
+                    }
+
+                    if (startSuccess)
+                    {
+                        // 성공적으로 시작된 경우 원격 포트 업데이트
+                        this.Invoke((Action)(() =>
+                        {
+                            textBoxRemotePort.Text = remotePort.ToString();
+                        }));
+
+                        // 성공 후 지속적으로 로그 모니터링
+                        while (!frpProcess.HasExited)
+                        {
+                            if (bgWorker.CancellationPending || stopRequested)
+                                break;
+                            string line = frpProcess.StandardOutput.ReadLine();
+                            if (line == null) break;
+                            HandleLogLine(line);
+                        }
+
+                        // 종료 시까지 기다림
+                        frpProcess.WaitForExit();
+                    }
+                }
+            }
+
+            if (!portAssigned && !stopRequested)
+            {
+                ReportLog("frpc를 시작할 수 없습니다. 재시도 횟수를 초과했습니다.\n");
+            }
+            else if (portAssigned && !stopRequested)
+            {
+                ReportLog($"원격 포트(remote_port): {textBoxRemotePort.Text}로 frpc가 시작되었습니다.\n");
+            }
+        }
+
+        private void HandleLogLine(string line)
+        {
+            line = RemoveAnsiEscape(line);
+            // 상태 파싱
+            if (line.Contains("start frpc service for config file"))
+                ReportLog("frpc 서비스를 시작합니다.\n");
+            else if (line.Contains("try to connect to server"))
+                ReportLog("서버에 연결을 시도합니다...\n");
+            else if (line.Contains("login to server success"))
+                ReportLog("서버에 성공적으로 로그인했습니다.\n");
+            else if (line.Contains("start proxy success"))
+                ReportLog("프록시가 성공적으로 시작되었습니다.\n");
+            else if (line.Contains("client exit success"))
+                ReportLog("클라이언트가 성공적으로 종료되었습니다.\n");
+
+            // 일반 로그 라인도 출력
+            ReportLog(line + "\n");
+        }
+
+        private string RemoveAnsiEscape(string line)
+        {
+            var ansiEscape = new Regex(@"\x1B\[[0-?]*[ -/]*[@-~]");
+            return ansiEscape.Replace(line, "");
+        }
+
+        private void ReportLog(string message)
+        {
+            if (bgWorker.CancellationPending) return;
+            bgWorker.ReportProgress(0, message);
+        }
+
+        private void BgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string msg = e.UserState as string;
+            if (msg != null)
+            {
+                richTextBoxOutput.AppendText(msg);
+                richTextBoxOutput.SelectionStart = richTextBoxOutput.Text.Length;
+                richTextBoxOutput.ScrollToCaret();
+            }
+        }
+
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!stopRequested)
+            {
+                buttonStart.Enabled = true;
+                buttonStop.Enabled = false;
+            }
         }
     }
 }
